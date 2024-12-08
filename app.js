2460de04ca335b810529ec8807ee8ec5b612d68b
Charcoal00 <wisestar175@gmail.com>
2024-12-08 04:59:58 +0100

    reset the required cors option

--- app.js
+++ app.js
@@ -9,13 +9,15 @@ const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
-// app.use(cors({
-//     origin: 'http://127.0.0.1:5500',
-//     methods:['GET', 'POST'],
-//     credentials: true,
-// }))
 
+// app.use(cors());
+
+const corsOptions = {
+    origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOptions));
 
 
 // Connect to MongoDB
